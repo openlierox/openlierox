b954c20f287086b319108fd59f33cd862e853b3e
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-06-09 12:58:19 +0300

    Script to build and run dedicated server on fresh debian installation

---
+++ tools/DedicatedServerSetupDebian/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo bash -c "apt-get install -y build-essential git cmake libsdl1.2-dev libsdl-mixer1.2-dev libsdl-image1.2-dev \
+  libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev libgd2-noxpm-dev \
+  zlib1g-dev libzip-dev libxml2-dev libcurl4-gnutls-dev \
+  libboost-dev libboost-system-dev libopenal-dev libalut-dev libvorbis-dev ; \
+  apt-get build-dep -y python3"
+
+curl -fsSL https://pyenv.run | bash
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+
+pyenv install 2.7.18
+
+cmake -D HAWKNL_BUILTIN=1 -D DEBUG=0 -D DEDICATED_ONLY=1 .
+
+make -j `nproc`
+
+cd tools/UDPMasterServer
+./compile.sh
+cd ../..
+
+{
+  crontab -l
+  echo ''
+  echo '@reboot while true; do $HOME/openlierox/tools/UDPMasterServer/bin/udpmasterserver; sleep 1; done'
+  echo '@reboot while true; do $HOME/openlierox/tools/UDPMasterServer/bin/udpmasterserver -6; sleep 1; done'
+  echo '@reboot export PATH=$HOME/.pyenv/versions/2.7.18/bin:$PATH; cd $HOME/openlierox/share/gamedir; while true; do ../../bin/openlierox 2>&1 | logger --tag openlierox; sleep 10; done'
+  echo '0 */6 * * * killall openlierox; sleep 1; killall -9 openlierox'
+} | crontab -
