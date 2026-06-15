99e5942897d36d5f805e198230c100973334d5b7
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-06-15 18:43:04 +0300

    Replaced URL to get external ip because openlierox.net does not have it anymore

--- src/server/CServer.cpp
+++ src/server/CServer.cpp
@@ -287,7 +287,7 @@ void GameServer::ObtainExternalIP()
 		return;
 
 	// TODO: use a config
-	tHttp2.RequestData("http://www.openlierox.net/external_ip.php", tLXOptions->sHttpProxy);
+	tHttp2.RequestData("https://ipinfo.io/ip", tLXOptions->sHttpProxy);
 }
 
 void GameServer::ProcessGetExternalIP()
