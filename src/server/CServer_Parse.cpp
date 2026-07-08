70cb20cdfee1f90b5e57be29979c138b04f1faed
Albert Zeyer <albzey@gmail.com>
2026-07-08 09:29:23 +0200

    Use ChallengeToSet directly, scope the loop index locally
    The loop counter i was reused after the loop only to carry
    ChallengeToSet down to the final tChallenges[i] read, which is exactly
    the aliasing that made the original out-of-bounds bug easy to miss.
    Read tChallenges[ChallengeToSet] directly and scope i to the for loop,
    so there is a single name for the selected slot.

--- src/server/CServer_Parse.cpp
+++ src/server/CServer_Parse.cpp
@@ -1024,7 +1024,6 @@ void GameServer::ParseConnectionlessPacket(const SmartPointer<NetworkSocket>& tS
 ///////////////////
 // Handle a "getchallenge" msg
 void GameServer::ParseGetChallenge(const SmartPointer<NetworkSocket>& tSocket, CBytestream *bs_in) {
-	int			i;
 	NetworkAddr	adrFrom;
 	AbsTime		OldestTime = AbsTime::Max();
 	int			ChallengeToSet = -1;
@@ -1082,14 +1081,13 @@ void GameServer::ParseGetChallenge(const SmartPointer<NetworkSocket>& tSocket, C
 	}
 	
 	// see if we already have a challenge for this ip
-	for (i = 0;i < MAX_CHALLENGES;i++) {
+	for (int i = 0;i < MAX_CHALLENGES;i++) {
 
 		if (IsNetAddrValid(tChallenges[i].Address)) {
 			if (AreNetAddrEqual(adrFrom, tChallenges[i].Address)) {
 				// We already have a challenge for this address, reuse the slot.
 				// (Skipping it instead would, once every slot holds this address,
-				// leave ChallengeToSet at -1 and i at MAX_CHALLENGES,
-				// so the tChallenges[i] read below would be out of bounds.)
+				// leave ChallengeToSet at -1, i.e. no slot selected.)
 				ChallengeToSet = i;
 				break;
 			}
@@ -1104,8 +1102,8 @@ void GameServer::ParseGetChallenge(const SmartPointer<NetworkSocket>& tSocket, C
 	}
 
 	// The loop above always selects a slot, so ChallengeToSet is >= 0 here.
-	// Guard anyway, so tChallenges[i] below can never be indexed out of
-	// bounds (with i == MAX_CHALLENGES) should that ever change.
+	// Guard anyway, so the tChallenges[ChallengeToSet] accesses below never
+	// use an invalid index should that ever change.
 	if (ChallengeToSet < 0) {
 		errors << "GameServer::ParseGetChallenge: no challenge slot selected" << endl;
 		return;
@@ -1117,8 +1115,6 @@ void GameServer::ParseGetChallenge(const SmartPointer<NetworkSocket>& tSocket, C
 	tChallenges[ChallengeToSet].fTime = tLX->currentTime;
 	tChallenges[ChallengeToSet].sClientVersion = client_version;
 
-	i = ChallengeToSet;
-
 	// Send the challenge details back to the client
 	tSocket->setRemoteAddress(adrFrom);
 
@@ -1126,7 +1122,7 @@ void GameServer::ParseGetChallenge(const SmartPointer<NetworkSocket>& tSocket, C
 	// TODO: move this out here
 	bs.writeInt(-1, 4);
 	bs.writeString("lx::challenge");
-	bs.writeInt(tChallenges[i].iNum, 4);
+	bs.writeInt(tChallenges[ChallengeToSet].iNum, 4);
 	if( client_version != "" )
 		bs.writeString(GetFullGameName());
 	bs.Send(tSocket.get());
