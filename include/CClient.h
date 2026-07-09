7e62dac4a8e8acf5275ebf4d22d998429d31f852
Albert Zeyer <albzey@gmail.com>
2026-07-09 17:51:53 +0200

    Pass weapon by const ref in Explosion/InjureWorm
    Explosion and InjureWorm took the weapon name by value.
    Explosion copies it on entry,
    then calls InjureWorm for each worm in the blast,
    copying it again per injured worm.
    Explosions fire per exploding projectile,
    so rapid or explosive weapons caused many std::string heap copies per frame.
    
    Take the weapon name by const reference in both.

--- include/CClient.h
+++ include/CClient.h
@@ -426,8 +426,8 @@ public:
 
 	void		SpawnProjectile(CVec pos, CVec vel, int rot, int owner, proj_t *_proj, int _random, AbsTime time, AbsTime ignoreWormCollBeforeTime);
     void        disableProjectile(CProjectile *prj);
-	void		Explosion(AbsTime time, CVec pos, float damage, int shake, int owner, std::string weapon = "");
-	void		InjureWorm(CWorm *w, float damage, int owner, std::string weapon = "");
+	void		Explosion(AbsTime time, CVec pos, float damage, int shake, int owner, const std::string& weapon = "");
+	void		InjureWorm(CWorm *w, float damage, int owner, const std::string& weapon = "");
 	void		LaserSight(CWorm *w, float Angle, bool highlightCrosshair = true);
 
 	void		processChatter();
